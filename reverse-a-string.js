96901827e34c2868580e008224cbf704cac56c03
Jamie Evans <taichi.chikung@gmail.com>
2024-08-11 00:55:14 +0100

    Updated Function

--- reverse-a-string.js
+++ reverse-a-string.js
@@ -1,5 +1,18 @@
+// function reverseString(str) {
+//   return str.split("").reverse().join("")
+// }
+
+/**
+ * 
+ * @param {This is a single loop varient} str 
+ * @returns 
+ */
 function reverseString(str) {
-  return str.split("").reverse().join("")
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
 }
 
 reverseString("hello");
