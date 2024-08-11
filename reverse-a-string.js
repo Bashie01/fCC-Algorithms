f2ffb1daebddf356f2c4d87f35a5e7c31cbb82fc
Jamie Evans <taichi.chikung@gmail.com>
2024-08-11 01:00:05 +0100

    Added function on my branch

--- reverse-a-string.js
+++ reverse-a-string.js
@@ -2,17 +2,12 @@
 //   return str.split("").reverse().join("")
 // }
 
-/**
- * 
- * @param {This is a single loop varient} str 
- * @returns 
- */
 function reverseString(str) {
-  let reversedStr = "";
+  let reversed = "";
   for (let i = str.length - 1; i >= 0; i--) {
-    reversedStr += str[i];
+    reversed += str[i];
   }
-  return reversedStr;
+  return reversed;
 }
 
 reverseString("hello");
