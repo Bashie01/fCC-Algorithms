5816b5a54c1a97bd0d4418ac468c254c0b128e7c
Bashie01 <aaron.jeffries@hotmail.co.uk>
2024-08-12 09:36:23 +0100

    Add files via upload

---
+++ finders-keepers.js
@@ -0,0 +1,12 @@
+function findElement(arr, func) {
+  let num = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if (func(num)){
+      return num;
+    }
+  }
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
